fd0305ad3ed0780e18e84cdb14322246ee53b58f
MarcBernstein0 <marcseth.bernstein@gmail.com>
2019-01-26 18:53:49 -0500

    Got ml to work using open source model, but need to increase word size of text generated

--- lyric_generator/create_dataset.py
+++ lyric_generator/create_dataset.py
@@ -1,42 +1,50 @@
-import tensorflow as tf 
+from textgenrnn import textgenrnn 
+from keras.utils.data_utils import get_file
 
-
+textgen = textgenrnn()
 
 def gen_2dlist():
 	song = []
 	file = open("not_afraid.txt", "r")
-	line = []
-	for word in file.read().split():
-		line.append(word)
-		if len(line) == 10:
-			song.append(line)
-			line = []
+	for line in file:
+		song.append(line.replace('\n', ''))
+		
 
 	file.close()
 	return song
 
-
-
-def my_func(arg):
-	arg = tf.convert_to_tensor(arg, dtype=tf.string)
-	return arg
+def gen_lyrics(arg):
+	textgen.train_on_texts(arg, num_epochs=20, gen_epochs=20)
 
 
 
 
+def new_model():
+	textgen.reset()
+	textgen.train_from_file('not_afraid.txt',
+							 new_model=True,
+							 rrn_bidirectional=True,
+							 dim_embeddings=300,
+							 num_epochs=5)
+
+	print(textgen.model.summary())
 
 
 
-
+def large_files():
+	text = get_file('not_afraid.txt', origin='https://www.google.com/search?q=not+afraid+lyrics&rlz=1C1CHBF_enUS719US719&oq=not+a&aqs=chrome.0.69i59j69i60l2j69i57j69i60j0.1472j0j1&sourceid=chrome&ie=UTF-8')
+	textgen.reset()
+	textgen.train_from_largetext_file(text, new_model=True, num_epochs=1)
 
 
 def main():
 	song = gen_2dlist()
-	print(song)
-	for l in song:
-		print(len(l))
-	tensor = my_func(song)
-	print(tensor)
+	print(type(song))
+	gen_lyrics(song)
+	# tensor = my_func(song)
+	# print(tensor)
+	# new_model()
+	# large_files()
 
 
 
