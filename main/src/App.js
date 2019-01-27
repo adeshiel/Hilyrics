7841a2ab1bf8707bef1d86ccdfabf0c88f6de93a
NaginiChen <naginichen@gmail.com>
2019-01-26 19:47:24 -0500

    React

--- main/src/App.js
+++ main/src/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
 import logo from './assets/logo.png';
 import './style/App.css';
-import {
-    BrowserRouter as Router,
-    Route,
-    Link
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 class App extends Component {
   render() {
     return (
-      <router>
-      <div class="flex_center">
-      <img src={logo} class="logo" />
-      <p class="fs_step" > To start, please login to your Spotify account. </p>
-      <p class="fs_step" > Choose your desired playlist.</p>
-      </div>
-      </router>
+      <Router>
+        <div>
+          <div class="flex_center">
+            <img src={logo} class="logo" />
+            <p class="fs_step" > To start, please login to your Spotify account. </p>
+          </div>
+
+          <div class="form_container">
+            <form class="flex_center">
+              <label>
+                Username:
+                <input type="text" name="name" />
+              </label>
+              <label>
+                Password:
+                <input type="Password" name="name" />
+              </label>
+                <input type="submit" value="Login" />
+            </form>
+
+            <Route path='/' component={Playlist}/>
+
+          </div>
+        </div>
+      </Router>
     );
   }
+
 }
+
+class Playlist extends Component{
+
+
+
+  render() {
+        return (...);
+  }
+  
+}
+
 
 export default App;
