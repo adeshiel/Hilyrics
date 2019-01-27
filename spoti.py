b96b9cbca261f4091209df6907644a0f4146ead8
Ashley Deshields <adeshiel@bu.edu>
2019-01-27 00:01:46 -0500

    emcommit

--- spoti.py
+++ spoti.py
@@ -28,49 +28,53 @@ genres = {}
 def getUsername():
     return "me"
 
-username = "naginichen"
+username = "ujustgotbernied"
 
 def runall():
+    authorize()
     token = util.prompt_for_user_token(username, 'playlist-read-private playlist-read-collaborative', client_id=C_ID, client_secret=SECRET, redirect_uri=redirect)
-    if token:
-        sp = spotipy.Spotify(auth=token)
-    else:
-        print("uhoh")
-    # authorize_path(a, sp)
-    getPlayLists(sp)
-    getChoiceList(name_to_id[list(name_to_id.keys())[0]], sp)
+    getPlayLists(token)
+    getChoiceList(name_to_id[list(name_to_id.keys())[0]], token)
     getLyrics()
 
+def authorize():
+    auth = oauth2.SpotifyOAuth(client_id=C_ID, client_secret=SECRET, redirect_uri=redirect, scope=scoped, cache_path='.spotipyoauthcache')
+    print(auth.get_authorize_url())
 
 
-def getPlayLists(sp):
-    print(sp.user(username))
-    results = sp.current_user_playlists()
-    for i in results['items']:
-        play_names.append(i['name'])
-        name_to_id[i['name']] = i['id']
+def getPlayLists(play_token):
+    if play_token:
+        sp = spotipy.Spotify(auth=play_token)
 
-    return play_names
+        print(sp.user(username))
+        results = sp.user_playlists(username)
+        for i in results['items']:
+            play_names.append(i['name'])
+            name_to_id[i['name']] = i['id']
 
-def getChoiceList(id, sp):
+        return play_names
 
-    list = sp.user_playlist_tracks(username, playlist_id=id, limit=2)
+def getChoiceList(id, play_token):
+    if play_token:
+        sp = spotipy.Spotify(auth=play_token)
+        list = sp.user_playlist_tracks(username, playlist_id=id, limit=2)
 
-    # print(list)
-    for song in list['items']:
-        cur = []
-        cur.append(song['track']['name'])
-        for help in song['track']['artists']:
-            if help['type'] == "artist":
-                cur.append(help['name'])
-        arttitle.append(cur)
+        # print(list)
+        for song in list['items']:
+            cur = []
+            cur.append(song['track']['name'])
+            for help in song['track']['artists']:
+                if help['type'] == "artist":
+                    cur.append(help['name'])
+            arttitle.append(cur)
 
-        # print(arttitle)
+        print(arttitle)
 
 
 
 def getLyrics():
     for item in arttitle:
+        # print(item[0])
         data = {
             'q_track': item[0],
             'q_artist': item[1],
@@ -82,40 +86,44 @@ def getLyrics():
         }
 
         res = req.get(mm_url + 'track.search', params=data)
+        # print(res)
         # t =
         jres = json.loads(res.text)
+        # print(jres)
         # print(jres['message']['body']['track_list'][0])
+        try:
+            short = jres['message']['body']['track_list'][0]
+            genre = short['track']['primary_genres']['music_genre_list'][0]['music_genre']['music_genre_name']
 
-        short = jres['message']['body']['track_list'][0]
+            if (genre in genres):
+                genres[genre] +=1
+            else:
+                genres[genre] = 1
+            track_id = short['track']['track_id']
+            # common_id = short['commontrack_id']
 
-        genre = short['track']['primary_genres']['music_genre_list'][0]['music_genre']['music_genre_name']
+            data2 = {
+                'track_id': track_id,
+                'apikey': MMKEY,
+            }
 
-        if (genre in genres):
-            genres[genre] +=1
-        else:
-            genres[genre] = 1
-        track_id = short['track']['track_id']
-        # common_id = short['commontrack_id']
+            res2 = req.get(mm_url + 'track.lyrics.get', params=data2)
+            jres2 = json.loads(res2.text)
 
-        data2 = {
-            'track_id': track_id,
-            'apikey': MMKEY,
-        }
+            lyrics = jres2['message']['body']['lyrics']['lyrics_body']
+            all_lyrics.append(lyrics[:-50])
+            print(lyrics[:-58])
 
-        res2 = req.get(mm_url + 'track.lyrics.get', params=data2)
-        jres2 = json.loads(res2.text)
 
-        lyrics = jres2['message']['body']['lyrics']['lyrics_body']
-        all_lyrics.append(lyrics[:-50])
-        print(lyrics[:-58])
+        except:
+            continue
 
-        return all_lyrics
 
+
+    return all_lyrics
 
 
 
 
 
 runall()
-
-# print(all_lyrics)
